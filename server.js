20c0af0fcb8ea85afd6aeeb0470b773492767dcf
Durgesh Dhoot <ddhoot@salesforce.com>
2020-04-04 18:13:57 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
-        
+var port = process.env.PORT || 8080;        
 var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hello World!!!');
+    res.writeHead(200);
+    res.end('Hello World!!!');
 });
-server.listen(8080);
+server.listen(port);
